72db9f0379c7b4604c523bb37537b2e7270295a0
LutfiCB <lutficahyob@gmail.com>
2023-02-16 22:36:23 +0700

    Update 16-02-2023

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -13,3 +13,14 @@ btn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+// Dynamic navbar
+var headerBottom = $("#header-bottom");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 80) {
+    headerBottom.addClass("header-bottom-aktif");
+  } else {
+    headerBottom.removeClass("header-bottom-aktif");
+  }
+});
